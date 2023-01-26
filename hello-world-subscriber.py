7ab49f1fd8665910480dbfc48bf10db391a09dea
Saber Be <saberbejestani@gmail.com>
2023-01-26 09:05:37 +0330

    create hello world subscriber

---
+++ hello-world-subscriber.py
@@ -0,0 +1,27 @@
+import time as t
+import json
+from mqttbrokers.AWSIoTV1 import AWSIoTV1Client
+from mqttbrokers.AWSIoTV2 import AWSIoTV2Client
+from mqttbrokers.paho_client import PAHOClient
+
+
+def on_message(msg):
+        print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+
+def run():
+    TOPIC = "test/testing"
+    
+    client = AWSIoTV1Client()
+    # uncomment the following line to use AWSIoTV2Client
+    client = AWSIoTV2Client()
+    # uncomment the following line to use PAHOClient
+    client = PAHOClient()
+    client.setup()
+    client.connect()
+    client.subscribe(TOPIC)
+    client.callback(on_message)
+    client.loop_forever()
+
+
+if __name__ == '__main__':
+    run()
